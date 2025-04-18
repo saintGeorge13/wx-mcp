1e1ad179e9ff95d583c1fb875951ab4edfcfbcb8
saintGeorge13 <49756844+saintGeorge13@users.noreply.github.com>
2025-04-18 23:14:54 +0800

    Add files via upload

---
+++ wx-mcp.py
@@ -0,0 +1,82 @@
+from mcp.server.fastmcp import FastMCP
+from wxauto import WeChat
+
+mcp = FastMCP("wxauto_mcp")
+wx = WeChat()
+
+
+@mcp.tool(name="send_message", description="send message to someone")
+def send_message(msg: str, to: str, at: str | list[str] = None):
+    try:
+        wx.SendMsg(msg, to, clear="True", at=at)
+    except Exception as e:
+        return f"发送消息失败: {str(e)}"
+    return "Message sent successfully"
+
+@mcp.tool(name="send_file", description="send file to someone")
+def send_files(files_path:str|list,nick_name:str):
+    '''发送文件(包含图片等文件)给指定的群或者个人'''
+    wx.SendFiles(files_path,nick_name)
+    return "ok"
+
+
+@mcp.tool(name="get_all_messages", description="get all messages from someone or group")
+def get_all_messages(who: str, load_more: bool = False):
+    try:
+        wx.ChatWith(who)
+        if load_more:
+            wx.LoadMoreMessage()
+        msgs = wx.GetAllMessage()
+        if msgs:
+            return [{'sender': msg.sender, 'content': msg.content} for msg in msgs if msg.type == 'friend']
+        return []
+    except Exception as e:
+        return f"获取消息失败: {str(e)}"
+
+
+import time
+
+
+@mcp.tool(name="auto_reply", description="auto reply customers")
+def auto_reply_new():
+    while True:
+        if wx.CheckNewMessage():
+            new_msgs = wx.GetNextNewMessage()
+            print(new_msgs)
+            for key, value in new_msgs.items():
+                if key != "Self":
+                    for new_msg in value:
+                        if '知识星球' in new_msg[1]:
+                            print(key)
+                            wx.SendMsg('请查看发货信息', key, clear="True")
+        time.sleep(5) # 每隔 5 秒检查一次新消息
+
+@mcp.tool(name="auto_friend_accept_and_message_reply", description="auto accept friend invitation and reply message")
+def auto_friend_accept_and_message_reply():
+    while True:
+        friend_list = wx.GetNewFriends()
+        ac_friend = []
+        for friend in friend_list:
+            if "AAA" in friend.msg:
+                friend.Accept(remark=friend.name)
+                ac_friend.append(friend)
+        for friend in ac_friend:
+            send_message("This is our city", friend.name)
+        if wx.CheckNewMessage():
+            new_msgs = wx.GetNextNewMessage()
+            print(new_msgs)
+            for key, value in new_msgs.items():
+                if key != "Self":
+                    for new_msg in value:
+                        if 'BBB' in new_msg[1]:
+                            print(key)
+                            wx.SendMsg('CCC', key, clear="True")
+        time.sleep(5) # 每隔 5 秒检查一次新消息
+
+
+if __name__ == "__main__":
+    mcp.run()
+
+
+
+
